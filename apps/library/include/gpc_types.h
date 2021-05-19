233f3e7b7ca2e28b9ddc17ab21683f0b75ff1781
DL8 <dl8.nir@gmail.com>
2021-05-19 15:21:37 +0300

    add gpc address type

--- apps/library/include/gpc_types.h
+++ apps/library/include/gpc_types.h
@@ -5,6 +5,21 @@
 #include "gpc_basic_types.h"
 
 /**
+ * Layout of a GPC core memory address
+ */
+typedef struct gpc_addr_s {
+    union {
+        struct {
+            uint32_t offset: 32 - 2 - GPC_AGENT_ID_BITS;
+            uint32_t region: 2;
+            uint32_t agent_id: GPC_AGENT_ID_BITS;
+        };
+        uint32_t raw;
+    };
+} gpc_addr_t;
+
+
+/**
  * Layout of core CR space
  **/
 typedef struct gpc_core_cr_s {
