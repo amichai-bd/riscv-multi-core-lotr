d7829f2753753b86126fc33767d643b0d2fccd22
DL8 <dl8.nir@gmail.com>
2021-05-28 02:08:00 +0300

    implement prime factoring
    * global init with input in global memory
    * local init each thread with local data as well
    * implement worker
    * manager polls workers and updates state
    * workers update state in global data, local data used for algorithm
    
    opens:
    * implement modulus (currently doesn't link)

--- apps/library/src/apps/prime_factoring.c
+++ apps/library/src/apps/prime_factoring.c
@@ -3,17 +3,49 @@
 #include "gpc_basic_types.h"
 #include "gpc_utils.h"
 
+_Static_assert(GPC_CORE_BOOTSTRAP_THREAD_ID == 0, "Bootstreap thread ID != 0");
+#define MANAGER_TID GPC_CORE_BOOTSTRAP_THREAD_ID
+
+typedef enum thread_state_e {
+    STATE_NOT_STARTED = 0,
+    STATE_START,
+    STATE_RUNNING,
+    STATE_FINISHED
+} thread_state;
+
 typedef struct global_s {
-    // channels[i][i] - current index of tid i. for master it's the number we factorize
-    // channels[i][j] - message from i to j
-    uint32_t channels[GPC_CORE_N_THREADS][GPC_CORE_N_THREADS];
+    uint32_t n; // number to factorize (input)
+    // thread state:
+    // bit 0: is_finished (0 - not finished, 1 - finished)
+    // bit 1: has_divisors (0 - no divisors found, 1 - divisors found)
+    // workers done when bit 0 is set for all
+    // prime if bit 1 is 0 for all
+    uint32_t state[GPC_CORE_N_THREADS];
 } global_t;
+
+typedef struct local_s {
+    uint32_t current; // current iteration value
+    uint32_t step; // step size
+} local_t;
 
 void gpc_global_setup(
     void *core_global, size_t core_global_size,
     volatile void *cr_space) {
     global_t *g = (global_t *) core_global;
     gpc_memset(g, sizeof(*g), 0);
+    // TODO: an ugly hack to pass input. real world code will need a better mechanism
+    // input was generated in a fair dice roll. guaranteed to be random
+    g->n = 4286436431;
+
+}
+
+static void manager_local_setup(global_t *g, local_t *l) {
+}
+
+static void worker_local_setup(uint32_t tid, global_t *g, local_t *l) {
+    l->current = (g->n >> 1) + 1;
+    l->current -= tid;
+    l->step = GPC_CORE_N_THREADS - 1;
 }
 
 void gpc_local_setup(uint32_t tid,
@@ -21,23 +53,49 @@ void gpc_local_setup(uint32_t tid,
     void *core_global, size_t core_global_size,
     volatile void *cr_space) {
     global_t *g = (global_t *) core_global;
-    // TODO: an ugly hack to pass input. real world code will need a better mechanism
-    // input was generated in a fair dice roll. guaranteed to be random
-    if(tid == GPC_CORE_BOOTSTRAP_THREAD_ID) {
-        g->channels[tid][tid] = 4286436431;
-        for(uint32_t i=0;i<GPC_CORE_N_THREADS;i++) {
-            if(i==tid) {
-                continue;
-            }
-            g->channels[tid][i] = 1;
-        }
+    local_t *l = (local_t *) tls;
+
+    gpc_memset(l, sizeof(*l), 0);
+
+    if(tid == MANAGER_TID) {
+        manager_local_setup(g, l);
     }
     else {
-        uint32_t upper_bound = gpc_wait_for_non_zero(&g->channels[GPC_CORE_BOOTSTRAP_THREAD_ID][GPC_CORE_BOOTSTRAP_THREAD_ID]);
-        upper_bound -= tid;
-        g->channels[tid][tid] = upper_bound;
-        // must wait for the master to start us
-        gpc_wait_for_non_zero(&g->channels[GPC_CORE_BOOTSTRAP_THREAD_ID][tid]);
+        worker_local_setup(tid, g, l);
+    }
+}
+
+static void manager_loop(global_t *g, local_t *l) {
+    uint32_t state = 1;
+    for(uint32_t i=0;i<GPC_CORE_N_THREADS;i++) {
+        if(i == MANAGER_TID) {
+            continue;
+        }
+        state &= ~(g->state[i] & 1);
+        state |= (g->state[i] & 2);
+    }
+    // bit 0 is 1 => someone is still running
+    // bit 1 is 0 => prime, 1 => composite
+    g->state[MANAGER_TID] |= (~state) & 1;
+    g->state[MANAGER_TID] |= state & 2;
+}
+
+static void worker_loop(uint32_t tid, global_t *g, local_t *l) {
+    if(l->current < 2) {
+        g->state[tid] |= 1;
+    }
+    if(g->state[tid] & 1) {
+        return;
+    }
+    // TODO: implement modulus
+    if(g->n % l->current != 0) {
+        g->state[tid] |= 3; // no need to go further - set as done and composite
+    }
+
+    if(l->current < l->step) {
+        l->current = 0;
+    } else {
+        l->current -= l->step;
     }
 }
 
@@ -45,10 +103,11 @@ void gpc_loop(uint32_t tid,
     void *tls, size_t tls_size,
     void *core_global, size_t core_global_size,
     volatile void *cr_space) {
-    if(tid == GPC_CORE_BOOTSTRAP_THREAD_ID) {
-        // TODO: poll threads
+    global_t *g = (global_t *) core_global;
+    local_t *l = (local_t *) tls;
+    if(tid == MANAGER_TID) {
+        manager_loop(g, l);
     } else {
-        // TODO: do calculation
-        // TODO: how do we calculate modulus without muldiv?
+        worker_loop(tid, g, l);
     }
 }
