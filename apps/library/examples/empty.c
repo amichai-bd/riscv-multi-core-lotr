f7d23be0e41a698c5e630afc5461f15def6551c0
DL8 <DL8@users.noreply.github.com>
2021-06-17 20:14:44 +0300

    Libgpc (#84)
    * restructure library code
    
    * src/apps -> examples/
    * src/lib -> src/
    * remove helpers.c (unused file)
    
    * define memory layout at build time
    
    * add gpc_memory_layout.h to describe memory layout with macros (default values)
    * create gpc_params_t, struct with GPC parameters
    * steal sizeof(gpc_params_t)*4 bytes from global data for GPC params per thread
    * update entry point to take build time defined offsets and sizes
    * update app entry point signatures to get pointer to gpc_params_t
    
    * add support for .rodata section in global data
    
    add .rodata section into global data in compile time
    
    * add global variables support
    
    open: doesn't link if .bss is placed inside .data. put into d_mem as workaround
    
    * add TOOLCHAIN_PREFIX knob to makefile
    
    to override default prefix, use:
    make TOOLCHAIN_PREFIX=[...]
    
    * add objcopy step to build process
    
    give sv data for each example (can be injected to testbench)
    
    * restructure library code
    
    * src/apps -> examples/
    * src/lib -> src/
    * remove helpers.c (unused file)
    
    * define memory layout at build time
    
    * add gpc_memory_layout.h to describe memory layout with macros (default values)
    * create gpc_params_t, struct with GPC parameters
    * steal sizeof(gpc_params_t)*4 bytes from global data for GPC params per thread
    * update entry point to take build time defined offsets and sizes
    * update app entry point signatures to get pointer to gpc_params_t
    
    * add support for .rodata section in global data
    
    add .rodata section into global data in compile time
    
    * add global variables support
    
    open: doesn't link if .bss is placed inside .data. put into d_mem as workaround
    
    * add TOOLCHAIN_PREFIX knob to makefile
    
    to override default prefix, use:
    make TOOLCHAIN_PREFIX=[...]
    
    * add objcopy step to build process
    
    give sv data for each example (can be injected to testbench)

---
+++ apps/library/examples/empty.c
@@ -0,0 +1,12 @@
+#include "gpc_entry.h"
+#include "gpc_defs.h"
+#include "gpc_basic_types.h"
+
+void gpc_global_setup(const gpc_params_t *gpc_params) {
+}
+
+void gpc_local_setup(const gpc_params_t *gpc_params) {
+}
+
+void gpc_loop(const gpc_params_t *gpc_params) {
+}
