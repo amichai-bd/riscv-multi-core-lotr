7f8be0742de82e56f2deb925795af944729484f7
amichai-bd <amichaibd@gmail.com>
2021-07-06 10:41:49 +0300

    module for the cr_mem.

--- source/gpc_4t/rtl/gpc_4t_param_pkg.sv
+++ source/gpc_4t/rtl/gpc_4t_param_pkg.sv
@@ -49,7 +49,8 @@ parameter SIZE_SHRD_MEM    = 2**(MSB_D_MEM );
 
 parameter MSB_CR                      = 7;
 // CR Address Offsets
-parameter CR_THREAD_ID_Q103H          = 12'h4  ;
+parameter CR_WHO_AM_I                 = 12'h0  ;
+parameter CR_THREAD_ID                = 12'h4  ;
 parameter CR_CORE_ID                  = 12'h8  ;
 parameter CR_STACK_BASE_OFFSET        = 12'hc  ;
 parameter CR_TLS_BASE_OFFSET          = 12'h10 ;
@@ -114,13 +115,12 @@ parameter LSB_CORE_ID      = 24;
 parameter MSB_CORE_ID      = 31;
 
 
-typedef struct packed { //RO
-    logic [31:0] pc;
+typedef struct packed { // RO\V
+    logic [9:0]  who_am_i;
     logic [1:0]  thread;
     logic [7:0]  core;
     logic [31:0] stk_ofst;
     logic [31:0] tls_ofst;
-    logic [31:0] shrd_ofst;
     logic [7:0]  i_mem_msb;
     logic [7:0]  d_mem_msb;
     logic [7:0]  sts_0;
@@ -135,31 +135,14 @@ typedef struct packed { //RO
     logic [31:0] pc_1;
     logic [31:0] pc_2;
     logic [31:0] pc_3;
-    logic [31:0] dfd_data_0;
-    logic [31:0] dfd_data_1;
-    logic [31:0] dfd_data_2;
-    logic [31:0] dfd_data_3;
         
 } t_cr_ro;
 
 typedef struct packed { //RW
-    logic en_pc;
-    logic rst_pc;
-    logic en_pc_0;
-    logic en_pc_1;
-    logic en_pc_2;
-    logic en_pc_3;
-    logic rst_pc_0;
-    logic rst_pc_1;
-    logic rst_pc_2;
-    logic rst_pc_3;
     logic [4:0]  dfd_id_0;
     logic [4:0]  dfd_id_1;
     logic [4:0]  dfd_id_2;
     logic [4:0]  dfd_id_3;  
-} t_core_cr;
-
-typedef struct packed { //RW
     logic [31:0] stk_ofst_0;
     logic [31:0] stk_ofst_1;
     logic [31:0] stk_ofst_2;
@@ -168,11 +151,22 @@ typedef struct packed { //RW
     logic [31:0] tls_ofst_1;
     logic [31:0] tls_ofst_2;
     logic [31:0] tls_ofst_3;    
-} t_cr_ofst;
+    logic [31:0] shrd_ofst;
+} t_cr_rw;
+
+typedef struct packed { //RW
+    logic        en_pc_0;
+    logic        en_pc_1;
+    logic        en_pc_2;
+    logic        en_pc_3;
+    logic        rst_pc_0;
+    logic        rst_pc_1;
+    logic        rst_pc_2;
+    logic        rst_pc_3;
+} t_core_cr;
 
 typedef struct packed { 
-    logic en_pc;
-    logic rst_pc;
+    logic who_am_i;
     logic en_pc_0;
     logic en_pc_1;
     logic en_pc_2;
@@ -193,7 +187,6 @@ typedef struct packed {
     logic tls_ofst_1;
     logic tls_ofst_2;
     logic tls_ofst_3; 
-    logic pc;
     logic thread;
     logic core;
     logic stk_ofst;
