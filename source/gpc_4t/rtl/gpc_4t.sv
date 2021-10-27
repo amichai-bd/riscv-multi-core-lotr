d258dd15908e1a0dba43923f728dc24d57a4c659
Adi Levy <adi.levy@campus.technion.ac.il>
2021-10-28 02:11:12 +0300

    WIP:CTF enabling first setup. need to discuss

--- source/gpc_4t/rtl/gpc_4t.sv
+++ source/gpc_4t/rtl/gpc_4t.sv
@@ -26,21 +26,25 @@ module gpc_4t
     input  logic          RstQnnnH            ,
     input  logic [7:0]    CoreID              ,
     //Core To Fabric(C2F)
+    //----input----
     input  logic          C2F_RspValidQ502H   ,  
     input  t_opcode       C2F_RspOpcodeQ502H  ,  
     input  logic [1:0]    C2F_RspThreadIDQ502H,  
     input  logic [31:0]   C2F_RspDataQ502H    ,
     input  logic          C2F_RspStall        ,
+    //----output----
     output logic          C2F_ReqValidQ500H   ,
     output t_opcode       C2F_ReqOpcodeQ500H  ,
     output logic [1:0]    C2F_ReqThreadIDQ500H,
     output logic [31:0]   C2F_ReqAddressQ500H ,
     output logic [31:0]   C2F_ReqDataQ500H    ,
     //Fabric To Core(F2C)
+    //----input----
     input  logic          F2C_ReqValidQ502H   ,
     input  t_opcode       F2C_ReqOpcodeQ502H  ,
     input  logic [31:0]   F2C_ReqAddressQ502H ,
     input  logic [31:0]   F2C_ReqDataQ502H    ,
+    //----output----
     output logic          F2C_RspValidQ500H   ,
     output t_opcode       F2C_RspOpcodeQ500H  ,
     output logic [31:0]   F2C_RspAddressQ500H ,
@@ -134,12 +138,26 @@ d_mem_wrap d_mem_wrap (
     //============================================
     //      RC interface
     //============================================
+    //F2C:
     .F2C_ReqValidQ503H    (F2C_ReqValidQ503H    ), // input 
     .F2C_ReqOpcodeQ503H   (F2C_ReqOpcodeQ503H   ), // input 
     .F2C_ReqAddressQ503H  (F2C_ReqAddressQ503H  ), // input 
     .F2C_ReqDataQ503H     (F2C_ReqDataQ503H     ), // input 
     .F2C_RspDMemValidQ504H(F2C_RspDMemValidQ504H), // output
-    .F2C_D_MemRspDataQ504H(F2C_D_MemRspDataQ504H)  // output
+    .F2C_D_MemRspDataQ504H(F2C_D_MemRspDataQ504H), // output
+    //C2F:
+    .C2F_RspValidQ502H      (C2F_RspValidQ502H   ),
+    .C2F_RspOpcodeQ502H     (C2F_RspOpcodeQ502H  ),
+    .C2F_RspThreadIDQ502H   (C2F_RspThreadIDQ502H),
+    .C2F_RspDataQ502H       (C2F_RspDataQ502H    ),
+    .C2F_RspStall           (C2F_RspStall        ),
+                           
+    .C2F_ReqValidQ500H      (C2F_ReqValidQ500H   ),
+    .C2F_ReqOpcodeQ500H     (C2F_ReqOpcodeQ500H  ),
+    .C2F_ReqThreadIDQ500H   (C2F_ReqThreadIDQ500H),
+    .C2F_ReqAddressQ500H    (C2F_ReqAddressQ500H ),
+    .C2F_ReqDataQ500H       (C2F_ReqDataQ500H    )
+    
 );
 
 //============================================
