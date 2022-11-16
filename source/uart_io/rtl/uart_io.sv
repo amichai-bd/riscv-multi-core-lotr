0329cd9d019b75fbf2a4d5fcee171e0d6d710d10
Chris Shakkour <chrisshakkour@gmail.com>
2022-11-16 19:04:34 +0200

    adds Core2RC and RC2Core interface on gateway module

--- source/uart_io/rtl/uart_io.sv
+++ source/uart_io/rtl/uart_io.sv
@@ -9,12 +9,36 @@
 
 module uart_io
   (
-   input logic 	clk,
-   input logic 	rstn,
+   input logic 	       clk,
+   input logic 	       rstn,
    // RC interface
+   // Core To Fabric(C2F)
+   // ----input----
+   input logic 	       C2F_RspValidQ502H, 
+   input 	       t_opcode C2F_RspOpcodeQ502H, 
+   input logic [1:0]   C2F_RspThreadIDQ502H, 
+   input logic [31:0]  C2F_RspDataQ502H,
+   input logic 	       C2F_RspStall,
+   // ----output----
+   output logic        C2F_ReqValidQ500H,
+   output 	       t_opcode C2F_ReqOpcodeQ500H,
+   output logic [1:0]  C2F_ReqThreadIDQ500H,
+   output logic [31:0] C2F_ReqAddressQ500H,
+   output logic [31:0] C2F_ReqDataQ500H,
+   // Fabric To Core(F2C)
+   //----input----
+   input logic 	       F2C_ReqValidQ502H,
+   input 	       t_opcode F2C_ReqOpcodeQ502H,
+   input logic [31:0]  F2C_ReqAddressQ502H,
+   input logic [31:0]  F2C_ReqDataQ502H,
+   //----output----
+   output logic        F2C_RspValidQ500H,
+   output 	       t_opcode F2C_RspOpcodeQ500H,
+   output logic [31:0] F2C_RspAddressQ500H,
+   output logic [31:0] F2C_RspDataQ500H 
    // uart RX/TX signals
-   input logic 	uart_master_tx, 
-   output logic uart_master_rx
+   input logic 	       uart_master_tx, 
+   output logic        uart_master_rx
    );
    
    logic 	interrupt;
